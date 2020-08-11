6ae5709bd69e8a83e9c86ab8c6f2c0a89033dd6c
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2020-08-11 21:16:57 +0200

    Update tokenPattern.py

--- tokenPattern.py
+++ tokenPattern.py
@@ -1,10 +1,12 @@
-
+import re
 # All the token matching should be done here - Basic rules #
 
-pattern = [
+patterns = [
     (r'[0-9]+','INT'),
     (r'\(','LP'),
     (r'\)','RP'),
     (r'\,','COMA'),
     ('[A-Za-z]','CHAR')
 ]
+
+patterns = [(re.compile(expr), name) for expr, name in patterns]
